c9aa7c860d4032266b85cfe2f9f37d7f7648366b
Paul Loyd <pavelko95@gmail.com>
2017-04-01 14:00:28 +0300

    feat(server): initial seeds

--- server/main.rs
+++ server/main.rs
@@ -1,17 +1,40 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
 extern crate common;
 #[macro_use]
 extern crate log;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate rocket;
+extern crate rocket_contrib;
 
-use std::thread;
-use std::time::Duration;
+use rocket::config::{Config, Environment};
+use rocket_contrib::Template;
 
 use common::logger;
+
+#[get("/")]
+pub fn index() -> Template {
+    #[derive(Serialize)]
+    struct Context {
+        name: String
+    }
+
+    Template::render("index", &Context {
+        name: "world".to_owned()
+    })
+}
 
 fn main() {
     logger::init().unwrap();
 
-    loop {
-        info!("Hello from server!");
-        thread::sleep(Duration::new(60, 0));
-    }
+    let config = Config::build(Environment::Production)
+        .port(3000)
+        .unwrap();
+
+    rocket::custom(config, false)
+        .mount("/", routes![index])
+        .launch();
 }
