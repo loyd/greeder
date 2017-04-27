ed8a2b802fba5799795e0d87095147033dbf9259
yandzee <yandzeek@gmail.com>
2017-04-28 00:34:23 +0300

    feat(server): controller for fetching all feeds

--- server/controllers/feed.rs
+++ server/controllers/feed.rs
@@ -8,9 +8,9 @@ use diesel::pg::PgConnection;
 use std::sync::Mutex;
 
 use common::types::{Url, Key};
-use common::models::{NewFeed, Feed};
+use models::{NewFeed, Feed, Subscription};
 use guards::user::UserGuard;
-use schema::feed;
+use schema::{feed, subscription};
 
 type Connection = Mutex<PgConnection>;
 
@@ -41,6 +41,38 @@ pub fn add(url: JSON<PostUrl>, user: UserGuard, conn: State<Connection>) -> Cust
         Ok(_) => Custom(Status::new(200, "Successfully added and subscribed"), ()),
         Err(_) => Custom(Status::new(500, "Addition failed"), ())
     }
+}
+
+type Feeds = JSON<Vec<Feed>>;
+
+#[get("/")]
+pub fn fetch_all(user: UserGuard, conn: State<Connection>) -> Custom<Feeds> {
+    let conn = conn.lock().unwrap();
+    use schema::subscription::dsl::user_id;
+    use schema::subscription;
+    let subscriptions = subscription::table
+        .filter(user_id.eq(user.0.id))
+        .load::<Subscription>(&*conn);
+    let subscriptions = match subscriptions {
+        Ok(subs) => subs,
+        Err(_) => return Custom(Status::new(500, "DB Error"), JSON(vec![]))
+    };
+
+    let mut feeds = vec![];
+    use schema::feed::dsl::id;
+    for sub in subscriptions {
+        let feed_id = sub.feed_id;
+        let part = feed::table
+            .filter(id.eq(feed_id))
+            .load::<Feed>(&*conn);
+        let part = match part {
+            Ok(data) => data,
+            Err(_) => return Custom(Status::new(500, "DB Error"), JSON(vec![]))
+        };
+        feeds.extend(part);
+    }
+
+    Custom(Status::Ok, JSON(feeds))
 }
 
 // #[delete("/remove",)]
