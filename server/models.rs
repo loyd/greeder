d36ac7e955931731809cea3e46b133ea5a0bb43e
yandzee <yandzeek@gmail.com>
2017-04-28 00:33:35 +0300

    wip(server): Feed model is serializable now

--- server/models.rs
+++ server/models.rs
@@ -4,6 +4,8 @@ use uuid::Uuid;
 use common::schema::{feed, user};
 use common::types::{Url, Key};
 
+use serde::ser::{Serialize, Serializer, SerializeStruct};
+
 #[derive(Debug, Queryable)]
 pub struct Feed {
     pub id: i32,
@@ -16,6 +18,27 @@ pub struct Feed {
     pub copyright: Option<String>,
     pub interval: Option<i32>,
     pub augmented: Option<Timespec>
+}
+
+impl Serialize for Feed {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer {
+        let mut state = serializer.serialize_struct("Feed", 5)?;
+        let title = match &self.title {
+            &Some(ref title) => title.clone(),
+            &None => String::new()
+        };
+        let description = match &self.description {
+            &Some(ref description) => description.clone(),
+            &None => String::new()
+        };
+        state.serialize_field("id", &self.id)?;
+        state.serialize_field("title", &title)?;
+        state.serialize_field("description", &description)?;
+        state.serialize_field("logo", &self.logo)?;
+        state.serialize_field("url", &self.url)?;
+        state.end()
+    }
 }
 
 #[derive(Debug, Insertable)]
