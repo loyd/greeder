bc8aeaa07b3475a6298df7ea311862f36e407a0f
yandzee <yandzeek@gmail.com>
2017-05-04 17:40:25 +0300

    fix(client): no templating on client

--- server/controllers/index.rs
+++ server/controllers/index.rs
@@ -1,6 +1,6 @@
 use std::path::{PathBuf, Path};
 use std::sync::Mutex;
-use std::io;
+use std::io::{self, Result as IoResult, ErrorKind};
 use rocket::http::{Cookies, Cookie};
 use rocket::State;
 use rocket::response::NamedFile;
@@ -10,51 +10,90 @@ use diesel;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 
-use models::{User, NewUser};
-use schema::user;
+use models::{User, NewUser, UserFeed, Feed, Subscription};
+use schema::{user, feed};
 
 type Connection = Mutex<PgConnection>;
-type IoResult<T> = io::Result<T>;
 
 #[derive(Serialize)]
 struct Context {
-    uid: String
+    uid: String,
+    feeds: Vec<UserFeed>
 }
 
 #[derive(Serialize)]
 struct EmptyContext;
 
-fn get_or_create_user(conn: &State<Connection>, cookies: &Cookies) -> IoResult<String> {
+fn get_or_create_user(conn: &State<Connection>, cookies: &Cookies) -> IoResult<User> {
     let conn = conn.lock().unwrap();
+    use schema::user::dsl::uid;
     if let Some(cookie) = cookies.find("uuid") {
-        Ok(cookie.value().to_owned())
+        let uuid = Uuid::parse_str(cookie.value()).unwrap();
+        let user = match user::table.filter(uid.eq(&uuid)).first::<User>(&*conn) {
+            Ok(user) => user,
+            Err(_) => return Err(io::Error::new(ErrorKind::Other, "Cant fetch user from DB"))
+        };
+        Ok(user)
     } else {
         let new_uuid = Uuid::new_v4();
         let new_user = NewUser { uid: new_uuid.clone() };
         let insertion = diesel::insert(&new_user)
             .into(user::table)
             .get_result::<User>(&*conn);
-
-        if let Ok(_) = insertion {
+        if let Ok(user) = insertion {
             let uuid_string = new_uuid.hyphenated().to_string();
             let cookie = Cookie::new("uuid", uuid_string.clone());
             cookies.add(cookie);
-            Ok(uuid_string)
+            Ok(user)
         } else {
-            Err(io::Error::new(io::ErrorKind::Other, "insertion error"))
+            Err(io::Error::new(ErrorKind::Other, "Cant save new user in DB"))
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct ErrorContext {
+    msg: String
+}
+
+impl ErrorContext {
+    fn new(e: &str) -> ErrorContext {
+        ErrorContext {
+            msg: e.to_string()
         }
     }
 }
 
 #[get("/")]
 pub fn index(conn: State<Connection>, cookies: &Cookies) -> Template {
-    let uid = match get_or_create_user(&conn, &cookies) {
+    let user = match get_or_create_user(&conn, &cookies) {
         Ok(uid) => uid,
-        _ => return Template::render("error", &EmptyContext)
+        Err(e) => return Template::render("error", &ErrorContext::new("Cant fetch user"))
+    };
+    let conn = conn.lock().unwrap();
+    use schema::subscription::dsl::user_id;
+    use schema::subscription;
+    use schema::feed::dsl::id as fid;
+
+    let subscriptions = subscription::table.filter(user_id.eq(&user.id)).load::<Subscription>(&*conn);
+    let subscriptions = match subscriptions {
+        Ok(subs) => subs,
+        Err(_) => return Template::render("error", &ErrorContext::new("Cant fetch subscriptions from DB" ))
     };
 
+    let mut feeds = vec![];
+    for sub in subscriptions {
+        let feed_id = sub.feed_id;
+        let part = match feed::table.filter(fid.eq(feed_id)).load::<Feed>(&*conn) {
+            Ok(data) => data.into_iter().map(|feed| feed.into()),
+            Err(_) => return Template::render("error", &ErrorContext::new("Cant fetch feeds from DB" ))
+        };
+        feeds.extend(part);
+    }
+
     Template::render("index", &Context {
-        uid: uid
+        uid: user.uid.hyphenated().to_string(),
+        feeds: feeds
     })
 }
 
